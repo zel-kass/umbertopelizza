ac673b13c37c149cf3399e7dedc9f0638a8e3adf
zelkass <zakary.elkassimi75015@gmail.com>
2025-03-08 17:13:11 +0100

    add: pre screen animation delay instead of on click

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 
 import NavBar from "@/app/components/NavBar";
@@ -9,43 +9,38 @@ import OptimizedVideo from "@/app/components/OptimizedVideo"
 export default function Home() {
 	const preScreenRef = useRef<HTMLDivElement>(null);
 	const contentDivRef = useRef<HTMLDivElement>(null);
-	const timelineRef = useRef<gsap.core.Timeline | null>(null);
 
 	useEffect(() => {
-		const tl = gsap.timeline({paused: true});
-		timelineRef.current = tl;
-
-		const viewportHeight = window.innerHeight;
-
-		gsap.set(contentDivRef.current, {
-			display: "none",
-		});
-
-		gsap.set(preScreenRef.current, {
-			opacity: 1,
-			y: 0,
-		});
-
-		tl.to(preScreenRef.current, {
-			y: -viewportHeight,
-			duration: 1.5,
-			ease: "power4.inOut",
-			onComplete: () => {
-				gsap.set(preScreenRef.current, {display: "none"});
-				gsap.set(contentDivRef.current, {display: "flex"});
-			},
-		});
-
-		return () => {
-			tl.kill();
-		};
-	}, [])
-
-	const handlePreScreenClick = () => {
-		if (timelineRef.current) {
-			timelineRef.current.play();
+		if (sessionStorage.getItem("isFirstVisit") === "true") {
+			const tl = gsap.timeline();
+			
+			const viewportHeight = window.innerHeight;
+			
+			gsap.set(contentDivRef.current, {
+				display: "none",
+			});
+			
+			gsap.set(preScreenRef.current, {
+				opacity: 1,
+				y: 0,
+			});
+			
+			tl.to(preScreenRef.current, {
+				y: -viewportHeight,
+				duration: 1.5,
+        delay: 0.5,
+				ease: "power4.inOut",
+				onComplete: () => {
+					gsap.set(preScreenRef.current, {display: "none"});
+					gsap.set(contentDivRef.current, {display: "flex"});
+				},
+			});
+			
+			return () => {
+				tl.kill();
+			};
 		}
-	}
+	}, [])
 
 	return (
 		<div className="min-h-screen flex flex-col">
@@ -53,7 +48,6 @@ export default function Home() {
 				className="absolute top-0 left-0 h-full w-full bg-white z-20 flex justify-center items-center text-8xl cursor-pointer overflow-hidden"
 				ref={preScreenRef}
 				id="pre-screen"
-				onClick={handlePreScreenClick}
 			>
 				<h1>PELIZZA</h1>
 			</div>
@@ -74,8 +68,8 @@ export default function Home() {
 				</div>
 				<div className="flex-col items-center justify-center px-8 h-screen uppercase gap-4" ref={contentDivRef}>
 					<h3 className="text-4xl">services</h3>
-					<h3 className="text-2xl leading-tight text-center">
-					nous sommes une agence créative avec un style visuel unique et une écoute attentive. Chaque projet est une collaboration : nous mêlons nos idées à vos besoins pour créer des contenus qui vous ressemblent. De la conception au tournage, jusqu&aposà la livraison finale, nous prenons en charge chaque étape pour vous offrir des formats sur-mesure, adaptés à vos envies et à votre univers.
+					<h3 className="text-2xl leading-tight text-center tracking-wide">
+					nous sommes une agence créative avec un style visuel unique et une écoute attentive. Chaque projet est une collaboration : nous mêlons nos idées à vos besoins pour créer des contenus qui vous ressemblent. De la conception au tournage, jusqu&apos;à la livraison finale, nous prenons en charge chaque étape pour vous offrir des formats sur-mesure, adaptés à vos envies et à votre univers.
 					</h3>
 				</div>
 			</main>
