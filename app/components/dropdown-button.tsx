f5b16e26b0a71e97353b3b7b89f62d00c774f48c
WallBlade <zakary.elkassimi75015@gmail.com>
2025-03-26 18:02:13 +0100

    add: animated dropdown menu

--- app/components/dropdown-button.tsx
+++ app/components/dropdown-button.tsx
@@ -5,42 +5,62 @@ import { useState, useEffect, useRef } from 'react';
 import AnimatedLink from "@/app/components/animated-link";
 
 export default function DropDownMenu () {
-  const worksContent = useRef(null)
-  const [isHovered, setIsHovered] = useState(false);
+  const worksContent = useRef<HTMLDivElement>(null)
+  const [isOpened, setIsOpened] = useState(false);
+	const tlRef = useRef<gsap.core.Timeline | null>(null)
 
   useEffect(() => {
-    gsap.set(worksContent.current, {
-      height: 0,
-      display: 'none',
+		if (!worksContent.current) return
+
+    if (tlRef.current) {
+      tlRef.current.kill()
+    }
+
+		tlRef.current = gsap.timeline({
+      paused: true,
+      onComplete: () => {
+        if (!isOpened) {
+          gsap.set(worksContent.current, { autoAlpha: 0, height: 0 })
+        }
+      },
     })
     
-    if (isHovered) {
-      console.log("YOUHOU")
-      gsap.to(worksContent.current, {
-        height: 'fit-content',
-        display: 'block'
-      })
-    }
-    // else {
-    //   gsap.to(worksContent.current, {
-    //     display: 'none',
-    //     height: 0
-    //   })
-    // }
-  }, [isHovered])
+		if (isOpened) {
+			gsap.set(worksContent.current, { autoAlpha: 0 })
+			tlRef.current.to(worksContent.current, {
+				autoAlpha: 1,
+				height: "auto",
+				duration: 0.5,
+				ease: "power2.out",
+			},)
+
+			tlRef.current.play()
+		} else {
+			const hasBeenOpened = worksContent.current.style.opacity !== "" && worksContent.current.style.opacity !== "0"
+			if (hasBeenOpened) {
+				tlRef.current
+					.to(worksContent.current, {
+						autoAlpha: 0,
+						height: 0,
+						duration: 0.5,
+						ease: "power2.in",
+					})
+					.play()
+			}
+		}
+  }, [isOpened])
 
   return (
     <div>
       <div
-        className='flex flex-col px-4 relative cursor-pointer hover:text-white hover:bg-zinc-900'
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
+        className='flex items-center gap-2 px-8 relative cursor-pointer hover:text-white hover:bg-zinc-900'
+        onClick={() => setIsOpened(!isOpened)}
       >
         <h1>WORKS</h1>
       </div>
-      <div className="absolute" ref={worksContent}>
-        <AnimatedLink link='/gallery' text="GALLERY"  />
-        <AnimatedLink link='/videos' text="VIDEOS"  />
+      <div className="invisible h-0 absolute flex flex-col gap-4 px-4 py-2 mt-4 bg-zinc-900 text-white" ref={worksContent}>
+        <AnimatedLink link='/gallery' text="GALLERY" />
+        <AnimatedLink link='/videos' text="VIDEOS" />
       </div>
     </div>
   )
