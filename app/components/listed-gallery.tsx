c10648cadeacf41e0407c3e3d7e8f51be2c87a6d
zelkass <zakary.elkassimi75015@gmail.com>
2025-04-08 17:54:09 +0200

    add: stagger animation on image placeholders

--- app/components/listed-gallery.tsx
+++ app/components/listed-gallery.tsx
@@ -22,59 +22,106 @@ const projects = {
   },
 }
 
+interface PhotoGalleryProps {
+	title: string;
+	photos: StaticImageData[];
+}
+
 export default function ListedGallery() {
-	const [counter, setCounter] = useState(1)
-	const observerRef = useRef<Observer | null>(null)
+	const [counter, setCounter] = useState(1);
+	const observerRef = useRef<Observer | null>(null);
+  const [isAnimating, setIsAnimating] = useState(false)
 
 	useEffect(() => {
 		observerRef.current = Observer.create({
 			target: window,
 			type: "wheel, touch, scroll",
 			onUp: () => {
-        if (counter === 1)
+        if (!isAnimating) {
+          if (counter === 1)
           setCounter(Object.keys(projects).length)
 				else
           setCounter(counter - 1)
+        } 
       },
       onDown: () => {
-				if (counter === Object.keys(projects).length)
-					setCounter(1)
-				else
-					setCounter(counter + 1)
+        if (!isAnimating) {
+          if (counter === Object.keys(projects).length)
+            setCounter(1)
+          else
+            setCounter(counter + 1)
+        }
       },
 		})
 	}, [counter])
 
-	const currentProject = projects[counter]
+  const currentProject = projects[counter as keyof typeof projects]
+  
+  useEffect(() => {
+    if (currentProject) {
+      setIsAnimating(true)
+
+      const tl = gsap.timeline({
+        onComplete: () => {
+          setIsAnimating(false)
+        }
+      })
+      
+      tl.to('#placeholder', {
+        height: '0',
+        duration: 0.5,
+        stagger: 0.1
+      })
+
+      tl.to('#placeholder', {
+        height: '100%',
+        duration: 0.5,
+        stagger: 0.1
+      })
+      
+    }
+  }, [counter, currentProject])
 
 	return (
-		<main className='flex h-full items-center justify-center'>
+		<main className='flex h-full w-full items-center justify-center relative'>
 			<PhotoGallery title={currentProject.name} photos={currentProject.photos} />
 		</main>
 	);
 }
 
-interface PhotoGalleryProps {
-	title: string;
-	photos: StaticImageData[];
-}
-
 function PhotoGallery({ title, photos }: PhotoGalleryProps) {
-	return (
-		<div className='flex flex-col gap-4 px-4'>
-			<h3>{title}</h3>
-			<div className='flex flex-col md:flex-row gap-2'>
-				{photos.map((photo, key) => (
-					<div key={key} className='relative'>
-						<Image
-							src={photo.src}
-							alt={`"jap_meta${key}"`}
-							width={photo.width}
-							height={photo.height}
-						/>
-					</div>
-				))}
-			</div>
-		</div>
-	)
+  useEffect(() => {
+  //   gsap.set("#placeholder", {
+  //     height: '0',
+  //   })
+
+    gsap.to("#placeholder", {
+      height: '100%',
+      duration: 1.5,
+      ease: "power2.out",
+      stagger: 0.1,
+      delay: 0.1,
+    })
+  }, [])
+
+  return (
+    <div className='flex flex-col gap-4 px-8 w-full'>
+      <h3>{title}</h3>
+      <div className='flex flex-col md:flex-row gap-2 h-[40vh] w-full'>
+        {photos.map((photo, key) => (
+          <div key={key} className='relative w-80' id='placeholder'>
+            <Image
+              src={photo.src}
+              alt={`"jap_meta${key}"`}
+              fill
+              sizes='30vh'
+              className='object-cover'
+              quality={100}
+              priority
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -17,7 +17,7 @@ export default function slideInOut() {
         transform: 'translateY(-35%)',
       }
     ], {
-      duration: 1500,
+      duration: 1000,
       easing: "cubic-bezier(0.87, 0, 0.13, 1)",
       fill: 'forwards',
       pseudoElement: "::view-transition-old(root)",
@@ -33,7 +33,7 @@ export default function slideInOut() {
         clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)"
       }
     ], {
-      duration: 1500,
+      duration: 1000,
       easing: "cubic-bezier(0.87, 0, 0.13, 1)",
       fill: 'forwards',
       pseudoElement: "::view-transition-new(root)",
