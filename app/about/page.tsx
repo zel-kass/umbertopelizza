3945aef05d0aee56e418f9008f2815c4ab8f09c1
zel-kass <76161335+zel-kass@users.noreply.github.com>
2026-09-14 13:54:21 +0200

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,20 +20,9 @@ export default function About() {
 							du processus créatif : direction artistique, écriture, production, réalisation 
 							et postproduction. Marques, artistes, médias ou organisateurs d’événements,
 							nous donnons vie à des contenus authentiques, efficaces et fidèles à votre vision.</p>
-						<p><b>PELIZZA PICTURES</b> is a bold, independent creative agency
-							specializing in the design and production of custom visual
-							and audiovisual content. From concept to delivery, we guide
-							every project with precision and an eye for detail—art
-							direction, writing, production, directing, and post-production.
-							Whether you are a brand, an artist, a media outlet or an
-							event organizer, we bring to life authentic, impactful content
-							that perfectly reflects your vision.</p>
-						<p><b>PELIZZA PICTURES</b><bdi lang='ja'>是一家风格鲜明的独立创意机构，专注于定制
-							化视觉与视听内容的创作与制作。 从创意到交付，我们以严谨和细致
-							的态度陪伴每一个项目：艺术指导、撰写、制作、拍摄以及后期制
-							作。 无论您是品牌、艺术家、媒体还是活动主办方，我们都能为您的
-							愿景打造真实且富有影响力的内容</bdi></p>
-						<p><bdi lang='ar'>تُعد</bdi><b> PELIZZA PICTURES </b><bdi lang='ar'> وكالة إبداعیة مستقلة ذات أسلوب ممیّز، متخصّصة في ابتكار وإنتاج محتوى بصري وسمعي-بصري مُصمَّم حسب الطلب. من الفكرة حتى التسلیم، نرافق كل مشروع بدقّة واھتمام بالتفاصیل: الإخراج الفني، الكتابة، الإنتاج، الإخراج، وما بعد الإنتاج. سواء كنتم علامة تجاریة، فنانًا، وسیلة إعلام أو منظّم فعالیة، فإننا نمنح رؤیتكم حیاةً من خلال محتوى .أصیل وفعّال ینسجم تمامًا مع تطلعاتكم</bdi></p>
+						<p><b>PELIZZA PICTURES</b> s an independent creative agency with a distinctive style, specializing in tailor-made visual and audiovisual content. A small, close-knit agency, we support each project from concept to delivery with rigor and attention to detail, managing the entire creative process: art direction, writing, production, filming, and post-production. Brands, artists, media outlets, or event organizers—we bring authentic, impactful content to life, faithful to your vision.</p>
+						<p><b>PELIZZA PICTURES</b><bdi lang='ja'>是一家独立创意机构，风格鲜明，专注于定制化的视觉和视听内容。作为一家家族式管理的创意机构，我们从概念到交付全程陪伴每一个项目，秉持严谨和对细节的关注，涵盖整个创作流程：艺术指导、文案创作、制作、拍摄及后期制作。无论是品牌、艺术家、媒体还是活动组织者，我们都致力于打造真实、有影响力、忠于您理念的内容。</bdi></p>
+						<p><bdi lang='ar'>تُعد</bdi><b> PELIZZA PICTURES </b><bdi lang='ar'>  هي وكالة إبداعية مستقلة ذات أسلوب مميز، متخصصة في إنتاج محتوى بصري وسمعي بصري حسب الطلب. كشركة عائلية صغيرة، ندعم كل مشروع من الفكرة إلى التسليم بدقة واهتمام بالتفاصيل، مع إدارة جميع مراحل العملية الإبداعية: الإخراج الفني، الكتابة، الإنتاج، التصوير، وما بعد الإنتاج. سواء كانت العلامات التجارية، الفنانون، وسائل الإعلام أو منظمو الفعاليات، نحن نحول أفكاركم إلى محتوى أصيل وفعّال يعكس رؤيتكم بدقة.</bdi></p>
 					</div>
 				</div>
 				<Footer />
